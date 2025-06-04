f84caefccab38bc532365f2d479b4b66262ef633
ManasCodeCraft <manassrivastava567@gmail.com>
2025-06-04 10:40:18 +0530

    updated

--- app/components/tools/toolComponentMap.ts
+++ app/components/tools/toolComponentMap.ts
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 import HtmlFormatter from "@/app/components/tools/Development/HtmlFormatter/HtmlFormatter";
 const MarkdownToHtml = dynamic(() => import("@/app/components/tools/Development/MarkdownToHtml/MarkdownToHtml"), { ssr: false });
 import TechDetector from "@/app/components/tools/Development/TechDetector/TechDetector";
-import ExifDataViewer from "@/app/components/tools/ImageGraphics/ExifDataViewer/ExifDataViewer";
+const ExifDataViewer = dynamic(() => import("@/app/components/tools/ImageGraphics/ExifDataViewer/ExifDataViewer"), { ssr: false });
 import ImageCompressor from "@/app/components/tools/ImageGraphics/ImageCompressor/ImageCompressor";
 import ImageUpscaler from "@/app/components/tools/ImageGraphics/ImageUpscaler/ImageUpscaler";
 import BinaryHexDecimalConverter from "@/app/components/tools/MathConversion/BinaryHexDecimalConverter/BinaryHexDecimalConverter";
